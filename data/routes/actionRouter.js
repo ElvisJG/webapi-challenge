f308962398a0d72a46ea37e771eb0dca4c06d17e
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-08-16 19:24:32 -0400

    ready to code, holy crap this is alot of boilerplate

--- data/routes/actionRouter.js
+++ data/routes/actionRouter.js
@@ -3,9 +3,44 @@ const express = require('express');
 const Action = require('../helpers/actionModel.js');
 const router = express.Router();
 
-router.get('', async (req, res) => {});
-router.post('', async (req, res) => {});
-router.put('', async (req, res) => {});
-router.delete('', async (req, res) => {});
+//
+router.get('', async (req, res) => {
+  try {
+  } catch (error) {
+    // log error
+    console.log(error);
+    res.status(500).json({ message: 'Error processing request' });
+  }
+});
+
+//
+router.post('', async (req, res) => {
+  try {
+  } catch (error) {
+    // log error
+    console.log(error);
+    res.status(500).json({ message: 'Error processing request' });
+  }
+});
+
+//
+router.put('', async (req, res) => {
+  try {
+  } catch (error) {
+    // log error
+    console.log(error);
+    res.status(500).json({ message: 'Error processing request' });
+  }
+});
+
+//
+router.delete('', async (req, res) => {
+  try {
+  } catch (error) {
+    // log error
+    console.log(error);
+    res.status(500).json({ message: 'Error processing request' });
+  }
+});
 
 module.exports = router;
--- data/routes/projectRouter.js
+++ data/routes/projectRouter.js
@@ -3,9 +3,54 @@ const express = require('express');
 const Project = require('../helpers/projectModel.js');
 const router = express.Router();
 
-router.get('', async (req, res) => {});
-router.post('', async (req, res) => {});
-router.put('', async (req, res) => {});
-router.delete('', async (req, res) => {});
+//
+router.get('', async (req, res) => {
+  try {
+  } catch (error) {
+    // log error
+    console.log(error);
+    res.status(500).json({ message: 'Error processing request' });
+  }
+});
+
+//
+router.get('', async (req, res) => {
+  try {
+  } catch (error) {
+    // log error
+    console.log(error);
+    res.status(500).json({ message: 'Error processing request' });
+  }
+});
+
+//
+router.post('', async (req, res) => {
+  try {
+  } catch (error) {
+    // log error
+    console.log(error);
+    res.status(500).json({ message: 'Error processing request' });
+  }
+});
+
+//
+router.put('', async (req, res) => {
+  try {
+  } catch (error) {
+    // log error
+    console.log(error);
+    res.status(500).json({ message: 'Error processing request' });
+  }
+});
+
+//
+router.delete('', async (req, res) => {
+  try {
+  } catch (error) {
+    // log error
+    console.log(error);
+    res.status(500).json({ message: 'Error processing request' });
+  }
+});
 
 module.exports = router;
