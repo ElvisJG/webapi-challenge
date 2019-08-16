3ed70152f24ee8e22885e8a5624db96e51379b89
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-08-16 19:07:26 -0400

    scaffolding

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const logger = require('morgan');
+
+const actionRouter = require('./data/routes/actionRouter');
+const projectRouter = require('./data/routes/projectRouter');
+
+const server = express();
+// Express.json is a method that returns a piece of middleware
+const bodyParser = express.json();
+
+// Routes
+server.use('/api/actions', actionRouter);
+server.use('/api/projects', projectRouter);
+
+// Middleware
+server.use(bodyParser);
+server.use(logger('dev'));
+
+server.get('/', (req, res) => {
+  res.send(`<h1>Hello World</h1>`);
+});
+
+module.exports = server;
