8d6049504f77b7e46d2265162ecbbb1b8ab86c41
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-08-16 19:20:48 -0400

    forgot to export router in routers :)

--- data/routes/actionRouter.js
+++ data/routes/actionRouter.js
@@ -7,3 +7,5 @@ router.get('', async (req, res) => {});
 router.post('', async (req, res) => {});
 router.put('', async (req, res) => {});
 router.delete('', async (req, res) => {});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const logger = require('morgan');
+const helmet = require('helmet');
 
 const actionRouter = require('./data/routes/actionRouter.js');
 const projectRouter = require('./data/routes/projectRouter.js');
@@ -8,16 +9,19 @@ const server = express();
 // Express.json is a method that returns a piece of middleware
 const bodyParser = express.json();
 
+// Middleware
+server.use(bodyParser);
+server.use(logger('dev'));
+server.use(helmet());
+
 // Routes
 server.use('/api/actions', actionRouter);
 server.use('/api/projects', projectRouter);
 
-// Middleware
-server.use(bodyParser);
-server.use(logger('dev'));
-
 server.get('/', (req, res) => {
-  res.send(`<h1>Hello World</h1>`);
+  res.send(`
+    <h2>WEB API Sprint Challenge</h2>
+    `);
 });
 
 module.exports = server;
