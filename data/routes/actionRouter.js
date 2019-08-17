100589591ee93e16ead71d955c81c06a38a99d91
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-08-16 20:22:52 -0400

    Added validation middleware

--- data/routes/actionRouter.js
+++ data/routes/actionRouter.js
@@ -29,20 +29,24 @@ router.get('/:id/projects', async (req, res) => {
 });
 
 //
-router.post('/:id/projects', async (req, res) => {
-  try {
-    const action = { ...req.body, project_id: req.params.id };
-    const addAction = await Action.insert(action);
-    res.status(201).json(addAction);
-  } catch (error) {
-    // log error
-    console.log(error);
-    res.status(500).json({ message: 'Error processing request' });
+router.post(
+  '/:id/projects',
+  [validateActions, validateProjectId],
+  async (req, res) => {
+    try {
+      const action = { ...req.body, project_id: req.params.id };
+      const addAction = await Action.insert(action);
+      res.status(201).json(addAction);
+    } catch (error) {
+      // log error
+      console.log(error);
+      res.status(500).json({ message: 'Error processing request' });
+    }
   }
-});
+);
 
 //
-router.put('/:id', async (req, res) => {
+router.put('/:id', [validateActions, validateProjectId], async (req, res) => {
   try {
     const update = await Action.update(req.params.id, req.body);
     res.status(200).json(update);
@@ -54,7 +58,7 @@ router.put('/:id', async (req, res) => {
 });
 
 //
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', validateProjectId, async (req, res) => {
   try {
     const destroy = await Action.remove(req.params.id);
     res.status(200).json(destroy);
@@ -64,5 +68,37 @@ router.delete('/:id', async (req, res) => {
     res.status(500).json({ message: 'Error processing request' });
   }
 });
+
+// Validation Middleware
+function validateActions(req, res, next) {
+  if (Object.keys(req.body.description).length > 128) {
+    res.status(400).json({ message: 'Why?... just... why?' });
+  } else if (!req.body.description) {
+    res.status(400).json({ message: 'missing required description field' });
+  } else if (!req.body.notes) {
+    res.status(400).json({ message: 'missing required notes field' });
+  } else {
+    next();
+  }
+}
+
+function validateProjectId(req, res, next) {
+  const { id } = req.params;
+
+  Project.getProjectActions(id)
+    .then(project => {
+      if (project) {
+        req.project_id = project;
+        next();
+      } else {
+        res.status(400).json({ message: 'invalid project ID' });
+      }
+    })
+    .catch(error => {
+      // log error
+      console.log(error);
+      res.status(500).json({ message: 'Error processing request' });
+    });
+}
 
 module.exports = router;
